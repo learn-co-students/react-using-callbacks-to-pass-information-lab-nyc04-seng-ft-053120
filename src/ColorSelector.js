c1582b90b0e775068251b370495bebc08157b27c
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-24 16:41:39 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
 
+  // add the props callback function as a callback function defined iwthin the map, can't just put this.props.setSelectedColor(str) in the onClick in the div--doesn't work, but you can create an anonymous arrow function in the onClick and pass the params that way
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      // let callback = () => this.props.setSelectedColor(str)
+      return <div onClick={() => this.props.setSelectedColor(str)} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
 
